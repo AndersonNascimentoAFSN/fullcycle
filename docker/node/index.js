336ed22a3e488a587af335f9ad7086a5cbada07a
Anderson Nascimento <anderson.nascimento@meta.com.br>
2022-11-22 22:06:43 -0300

    add depends_on

--- docker/node/index.js
+++ docker/node/index.js
@@ -3,6 +3,19 @@ const app = express()
 
 const port = 3000
 
+const config = {
+  host: 'dbnode',
+  user: 'root',
+  password: 'root',
+  database: 'nodedb'
+}
+const mysql = require('mysql')
+const connection = mysql.createConnection(config)
+const sql = `INSERT INTO people(name) value('Anderson')`
+connection.query(sql)
+connection.end()
+
+
 app.get('/', (req, res) => {
   res.send('<h1>Full Cycle</h1>')
 })
